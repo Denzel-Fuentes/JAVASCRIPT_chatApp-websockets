c0224819c0a70acce7c69f1c6827736d616d009e
Denzel Fuentes <denzel.fuentes.n@gmail.com>
2023-06-20 09:32:09 -0400

    agregando redis

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const passport = require('passport');
 const session = require('express-session');
 const flash = require('connect-flash');
+const Redis = require('ioredis');
+const RedisStore = require('connect-redis')(session);
 const Database = require('./database');
 const socketChat = require('./socket');
 require('./passport/local-auth');
@@ -24,11 +26,28 @@ class App {
     this.app.use(express.json());
     this.app.use(morgan('dev'));
     this.app.use(express.urlencoded({ extended: false }));
-    this.session= session({
+
+    // Configurar instancia de Redis
+    const redisClient = new Redis({
+      host: process.env.REDIS_HOST || '127.0.0.1',
+      port: process.env.REDIS_PORT || 6379,
+      password: process.env.REDIS_PASSWORD
+    });
+    redisClient.on('connect', () => {
+      console.log('Redis connected successfully');
+    });
+    
+    redisClient.on('error', (error) => {
+      console.error('Error connecting to Redis:', error);
+    });
+
+    this.session = session({
       secret: 'shinmegamitensei3',
       resave: false,
-      saveUninitialized: false
-    })
+      saveUninitialized: false,
+      store: new RedisStore({ client: redisClient })
+    });
+
     this.app.use(this.session);
     this.app.use(flash());
     this.app.use(passport.initialize());
@@ -39,7 +58,7 @@ class App {
       next();
     });
     this.app.use(require('./routes/index'));
-    this.app.use(require('./routes/mensajes'))
+    this.app.use(require('./routes/mensajes'));
   }
 
   start() {
@@ -47,8 +66,8 @@ class App {
       console.log(`Server running on port ${this.port}`);
     });
 
-    // Instancia de ChatSocket y pasa el servidor http
-     new socketChat(this.server,this.session);
+    // Instancia de ChatSocket y pasa el servidor http y la sesión
+    new socketChat(this.server, this.session);
   }
 }
 
